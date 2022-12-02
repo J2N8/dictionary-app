5f99d029c3d33c5c5efbf08742ad74d9a90cfa59
J2N8 <111210732+J2N8@users.noreply.github.com>
2022-12-01 22:22:27 -0500

    Added Header and an image to the background

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import "./App.css";
 import Dictionary from "./Dictionary";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
       <div className="container">
+        <header className="App-header">
+          <h1>Dic·tion·ar·y</h1>
+          <hr />
+          <h2>
+            If you have a big enough dictionary, just about everything is a
+            word. 
+          </h2>
+          <h3><em>- Dave Barry</em></h3>
+          <hr />
+        </header>
         <main>
           <Dictionary />
         </main>
@@ -15,5 +25,3 @@ function App() {
     </div>
   );
 }
-
-export default App;
