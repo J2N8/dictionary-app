05a17366758e201b62eb9466d394f106651c8cf7
J2N8 <111210732+J2N8@users.noreply.github.com>
2022-12-02 19:08:11 -0500

    Updated Footer

--- src/App.js
+++ src/App.js
@@ -10,16 +10,45 @@ export default function App() {
           <hr />
           <h3>
             If you have a big enough dictionary, just about everything is a
-            word. 
+            word.
           </h3>
-          <h3><em>- Dave Barry</em></h3>
+          <h3>
+            <em>- Dave Barry</em>
+          </h3>
           <hr />
         </header>
         <main>
-          <Dictionary defaultKeyword="sunrise" />
+          <Dictionary defaultKeyword="yoga" />
         </main>
         <footer className="App-footer">
-          <small>Coded by Jasmine Poole</small>
+          <section>
+            Coded by{" "}
+            <a
+              href="https://www.linkedin.com/in/jasmine-poole-421b101b7/"
+              target="_blank"
+              className="linkedIn"
+              rel="noopener noreferrer"
+            >
+              Jasmine Poole
+            </a>{" "}
+            and is{" "}
+            <a
+              href="https://github.com/J2N8/dictionary-app"
+              target="_blank"
+              className="github"
+              rel="noopener noreferrer"
+            >
+              open-sourced on GitHub,
+            </a>{" "}
+            <a
+              href="https://dictionaryapp-reactjs.netlify.app/"
+              target="_blank"
+              className="netlify"
+              rel="noopener noreferrer"
+            >
+              hosted on Netlify
+            </a>
+          </section>
         </footer>
       </div>
     </div>
